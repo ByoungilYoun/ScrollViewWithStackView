3d705b57c01195a01ca0de27dcb50b29976d52a6
YounByoungil <jimmy7525@naver.com>
2021-12-20 14:38:50 +0900

    Project Setting

---
+++ UIScrollViewWithStackView/UIScrollViewWithStackView/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  UIScrollViewWithStackView
+//
+//  Created by 윤병일 on 2021/12/20.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .white
+  }
+  
+  //MARK: - @objc func
+  
+}
